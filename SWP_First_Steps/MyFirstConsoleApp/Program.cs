bcd33c1e3fda09a28b0d140a66d70a7989281f34
FlorianFleisch <florian.fleisch@student.htldornbirn.at>
2024-09-20 10:06:33 +0200

    ignore

---
+++ SWP_First_Steps/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+Console.WriteLine("Enter Username");
+string userName = Console.ReadLine();
+Console.WriteLine("Username is: " + userName);
+Console.WriteLine("How old are you?");
+int alter = 0;
+alter = Int32.Parse(Console.ReadLine());
+if  (alter < 18)
+{
+    Console.WriteLine("Nicht alt genug");
+}else if (alter > 18)
+{
+    Console.WriteLine("Du Bist alt genug");
+}
+Console.WriteLine("Supper dann darfst du diesen Kontent gucken!!!");
