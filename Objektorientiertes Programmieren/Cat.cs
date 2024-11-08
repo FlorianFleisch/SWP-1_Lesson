8f9aebb87d2b95ba23e2f6c9fdffa0c6c212651d
FlorianFleisch <florian.fleisch@student.htldornbirn.at>
2024-11-08 12:34:42 +0100

    cat fertig

---
+++ Objektorientiertes Programmieren/Cat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Objektorientiertes_Programmieren
+{
+    class Cat
+    {
+
+        private string? _Color = null;
+        private DateTime _birthDate;
+
+        public string Color
+        {
+            get { return _Color; }
+            set
+            {
+                if (_Color == value) return;
+                _Color = value;
+            }
+        }
+
+        // Öffentliche Eigenschaft, um das Alter zu berechnen
+        public int Age
+        {
+            get { return DateTime.Now.Year - _birthDate.Year; }
+
+        }
+
+        // Konstruktor, um das Geburtsdatum festzulegen
+        public Cat(DateTime birthDate)
+        {
+            _birthDate = birthDate;
+        }
+    }
+}
---
+++ Objektorientiertes Programmieren/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Objektorientiertes_Programmieren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Erstellen eines Objekts der Klasse Cat
+            Cat myCat = new Cat(new DateTime(2022, 11, 8));
+            myCat.Color = "black";
+
+            // Aufrufen der Methode
+            Console.WriteLine("The Color of my Cat is " + myCat.Color + " and it is " + myCat.Age + " Years old...");  // Ausgabe: The color of my cat is black and she is 2 years old.
+        }
+    }
+}
